0106dcfe7d6f42a20cb75bea133691c43887a51a
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-03-01 11:29:59 -0600

    Set up routes in app.js and created component files for login, register and listing out the users in the components folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-
+import { Route, NavLink, withRouter } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+
+  signOut = e => {
+    e.preventDefault();
+    localStorage.removeItem('AuthToken');
+    this.props.history.push('/');
+  }
+
   render() {
     return (
       <div className="App">
+        <nav>
+          <button onClick={this.signOut}>Signout</button>
+          <NavLink to = "/users">Users</NavLink>
+        </nav>
+        <Route exact path = "/" component={} />
+        <Route path = "/register" component={} />
+        <Route path = "/users" component={} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
